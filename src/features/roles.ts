0fc6b06cb6d5731c41d65a4f2def96036183c9af
Not TCA <realtcatech@gmail.com>
2022-09-25 18:56:12 +0800

    added roles feature

---
+++ src/features/roles.ts
@@ -0,0 +1,128 @@
+import { Feature } from "../structures/Feature";
+import {
+  MessageCreateOptions,
+  TextChannel,
+  GuildMemberRoleManager,
+  ButtonInteraction,
+  EmbedBuilder,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+} from "discord.js";
+
+export default new Feature((client) => {
+  const roles: Object = {
+    upload: "1003518031365931008",
+    livestream: "1023503398751969352",
+    announcement: "1003658192762441738",
+    jipstats: "1000689439087013989",
+    graphify: "1023427951703900230",
+  };
+
+  // message sending and editing
+  client.on("ready", async () => {
+    const options: Array<MessageCreateOptions> = [
+      {
+        embeds: [
+          new EmbedBuilder()
+            .setTitle("🔔 Ping Roles")
+            .setDescription(
+              `<:uploadping:1017706882514833448> Upload Ping\n> Get pinged when JipStats or Graphify posts a new video.\n<:live:1023545225177747467> Livestream Ping\n> Get pinged whenever JipStats or Graphify starts a new livestream.\n📣 Announcement Ping\n> Get pinged whenever we annouce something.`
+            )
+            .setColor(client.config.color),
+        ],
+        components: [
+          new ActionRowBuilder<ButtonBuilder>().addComponents(
+            new ButtonBuilder()
+              .setCustomId("upload")
+              .setStyle(ButtonStyle.Secondary)
+              .setEmoji("1017706882514833448"),
+            new ButtonBuilder()
+              .setCustomId("livestream")
+              .setStyle(ButtonStyle.Secondary)
+              .setEmoji("1023545225177747467"),
+            new ButtonBuilder()
+              .setCustomId("announcement")
+              .setStyle(ButtonStyle.Secondary)
+              .setEmoji("📣")
+          ),
+        ],
+      },
+      {
+        embeds: [
+          new EmbedBuilder()
+            .setTitle("🤔 Which channel are you subscribed to?")
+            .setColor(client.config.color),
+        ],
+        components: [
+          new ActionRowBuilder<ButtonBuilder>().addComponents(
+            new ButtonBuilder()
+              .setCustomId("jipstats")
+              .setStyle(ButtonStyle.Secondary)
+              .setEmoji("🔴")
+              .setLabel("JipStats"),
+            new ButtonBuilder()
+              .setCustomId("graphify")
+              .setStyle(ButtonStyle.Secondary)
+              .setEmoji("🔵")
+              .setLabel("Graphify")
+          ),
+        ],
+      },
+    ];
+
+    const channel = (await client.channels.fetch(
+      client.config.channels.roles
+    )) as TextChannel;
+
+    channel?.messages.fetch().then((messages) => {
+      if (messages.size === 0) {
+        for (const option of options) {
+          channel.send(option);
+        }
+      } else {
+        for (const message of messages) {
+          message[1].delete();
+        }
+        for (const option of options) {
+          channel.send(option);
+        }
+      }
+    });
+  });
+
+  // button pressing
+  client.on("interactionCreate", (int) => {
+    if (!int.isButton()) return;
+    if (!roles.hasOwnProperty(int.customId)) return;
+    addRole(roles[int.customId], int);
+  });
+
+  function addRole(id: string, int: ButtonInteraction) {
+    const memberRoles = int.member.roles as GuildMemberRoleManager;
+
+    const role = int.guild.roles.cache.get(id);
+    if (memberRoles.cache.has(role.id)) {
+      int.reply({
+        embeds: [
+          new EmbedBuilder()
+            .setTitle(`❌ You already have the ${role.name} role.`)
+            .setColor("Red"),
+        ],
+        ephemeral: true,
+      });
+      return;
+    }
+
+    memberRoles.add(role);
+
+    int.reply({
+      embeds: [
+        new EmbedBuilder()
+          .setTitle(`✅ You now have the ${role.name} role!`)
+          .setColor("DarkGreen"),
+      ],
+      ephemeral: true,
+    });
+  }
+});
